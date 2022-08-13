12b0ec9c03079ee56eaf0ebdd39896133a5bc62b
LuisEJaar <luisjaarcoding@gmail.com>
2022-08-12 21:24:43 -0500

    Updated CSS to a good point, changed completely to rem

--- travel-journal/src/Card.js
+++ travel-journal/src/Card.js
@@ -1,25 +1,29 @@
 import React from 'react'
 
 export default function Card(props) {
+    let location = props.location
+    location.toUpperCase()
+    
+    
     return(
         <article>
             <div className='imageContainer'>
                 {/* image */}
-                <img src={props.imageUrl} alt={`${props.location} Picture`}/>
+                <img src={props.imageUrl} alt={`${location}`}/>
             </div>
             <div className='textContainer'>
                 {/* Country / maps */}
                 <div>
                     <i class="fa-solid fa-location-dot locationIcon"></i>
                     <span className='country'>{props.location} </span>
-                    <a href={`${props.googleMapsUrl}`}>View in google maps</a> 
+                    <a href={`${props.googleMapsUrl}`} target="_blank" rel="noreferrer">View in google maps</a> 
                 </div>
                 {/* Name of site */}
                 <div className='visited'>{props.title}</div>
                 {/* date of visit */}
                 <div className='date'>{props.startDate} - {props.endDate}</div>
                 {/* Description */}
-                <div>{props.description}</div>
+                <div className='description'>{props.description}</div>
             </div>
         </article>
     )
